75528e7247faa368686991583274efcffd70025c
thanhnhanggg <lephuocthanhnhat0409@gmail.com>
2026-06-23 13:56:39 +0700

    Update AppServiceProvider.php

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -21,5 +21,9 @@ class AppServiceProvider extends ServiceProvider
     public function boot(): void
     {
         CrawlTask::observe(CrawlTaskObserver::class);
+
+        if (config('app.env') === 'production') {
+            \Illuminate\Support\Facades\URL::forceScheme('https');
+        }
     }
 }
